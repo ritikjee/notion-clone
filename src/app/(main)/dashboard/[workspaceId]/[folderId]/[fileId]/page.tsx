0d5d972516d031e0cbaeea0df601d08cd269bd2b
ritikjee <ritikj.ug21.ee@nitp.ac.in>
2024-05-05 22:19:57 +0530

    Refactor file and folder page components

--- src/app/(main)/dashboard/[workspaceId]/[folderId]/[fileId]/page.tsx
+++ src/app/(main)/dashboard/[workspaceId]/[folderId]/[fileId]/page.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Editor from "@/components/editor";
+import { getFileDetails } from "@/lib/supabase/queries";
+import { redirect } from "next/navigation";
 
-function FilePage() {
-  return <div>File Page</div>;
+async function FilePage({ params }: { params: { fileId: string } }) {
+  const { error, data } = await getFileDetails(params.fileId);
+
+  if (error || !data.length) redirect("/dashboard");
+
+  return (
+    <div className="relative">
+      <Editor
+        dirDetails={data[0] || {}}
+        dirType="folder"
+        fileId={params.fileId}
+      />
+    </div>
+  );
 }
 
 export default FilePage;
--- src/app/(main)/dashboard/[workspaceId]/[folderId]/page.tsx
+++ src/app/(main)/dashboard/[workspaceId]/[folderId]/page.tsx
@@ -1,7 +1,26 @@
+import Editor from "@/components/editor";
+import { getFolderDetails } from "@/lib/supabase/queries";
+import { redirect } from "next/navigation";
 import React from "react";
 
-function FolderPage() {
-  return <div>Folder Page</div>;
+async function FolderPage({
+  params,
+}: {
+  params: { folderId: string; workspaceId: string };
+}) {
+  const { error, data } = await getFolderDetails(params.folderId);
+
+  if (error || !data.length) redirect("/dashboard");
+
+  return (
+    <div className="relative">
+      <Editor
+        dirDetails={data[0] || {}}
+        dirType="folder"
+        fileId={params.folderId}
+      />
+    </div>
+  );
 }
 
 export default FolderPage;
